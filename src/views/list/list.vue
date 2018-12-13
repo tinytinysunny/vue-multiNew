7238c3527d9b8ecc9f6274a96d4d0fb7b098e9b8
孙道晓 <sundaoxiao@ymatou.com>
2018-12-13 14:34:46 +0800

    sve

--- src/views/list/list.vue
+++ src/views/list/list.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>how are you</div>
+    <div class="wrap_all">
+        <div>我我我哦我我我</div>
+        <div>你你你你你你你</div>
+        <div>他他他他他他他</div>
+    </div>
 </template>
 <script>
 export default {
@@ -10,6 +14,10 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .wrap_all{
+    div{
+      color:yellowgreen;
+    }
+  }
 </style>
